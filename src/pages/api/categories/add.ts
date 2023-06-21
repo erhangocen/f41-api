e259095866e31a023123e96b99b894918a00bac2
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-06-22 00:52:16 +0300

    update category

--- src/pages/api/categories/add.ts
+++ src/pages/api/categories/add.ts
@@ -6,15 +6,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
-    const { name } = req.body;
+    const { name, photoUrl } = req.body;
 
     try {
-        if (!req.body || !name) {
+        if (!req.body || !name || !photoUrl) {
             return res.status(400).json({ "error": "fields cannot be empty" });
         }
         await db.category.create({
             data: {
-                name: name
+                name: name,
+                photoUrl: photoUrl
             },
         })
         return res.status(200).json(createResponseData("Category successfully added!"));
