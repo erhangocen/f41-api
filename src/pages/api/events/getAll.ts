87eb28fda3191e5d87e0c1662bbfc13f22830016
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-05 19:42:33 +0300

    .

---
+++ src/pages/api/events/getAll.ts
@@ -0,0 +1,23 @@
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { db } from "@/lib/db";
+import RequestMethods from "@/lib/request_methods";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    try {
+        const responseData = await db.event.findMany({
+            include: {
+                city: { include: { country: true } },
+                eventAttendees: { include: { user: true } },
+                userLikeEvents: { include: { user: true } },
+                group: { include: { category: true, owner: true } },
+            }
+        });
+        return res.status(200).json(responseData);
+    } catch (error) {
+        return res.status(500).json({ error: error })
+    }
+}
+
+export default withMethods([RequestMethods.GET], handler, false)
