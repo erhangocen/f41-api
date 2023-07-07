2f275df87556f63b6e1ab265c81aa1cd2f29b085
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-07 04:30:15 +0300

    .

--- src/pages/api/events/getAll.ts
+++ src/pages/api/events/getAll.ts
@@ -16,6 +16,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 userLikeEvents: { include: { user: true } },
                 group: { include: { category: true, owner: true } },
             },
+            orderBy: { eventDate: 'asc' },
             where: {
                 eventDate: {
                     gt: new Date(Date.now()).toISOString(),
