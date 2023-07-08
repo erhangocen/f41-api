ce1c52fe41dcaf915ad779b90fc1f19b336f0b0a
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-08 21:57:33 +0300

    .

---
+++ src/pages/api/groups/getByAttendedUser.ts
@@ -0,0 +1,26 @@
+import { withMethods } from "@/lib/api-middlewares/with-methods";
+import { db } from "@/lib/db";
+import RequestMethods from "@/lib/request_methods";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const userId = req.query.userId?.toString();
+
+    try {
+        const responseData = await db.group.findMany({
+            where: {
+                userGroups: {
+                    some: {
+                        userId: userId ?? "",
+                    }
+                }
+            }
+        });
+        return res.status(200).json(responseData);
+    } catch (error) {
+        return res.status(500).json({ error: error })
+    }
+}
+
+export default withMethods([RequestMethods.GET], handler, false)
