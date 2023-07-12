b5c35ef1b87f29246d77887614343c40b28a21ee
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-12 23:55:24 +0300

    .

--- src/pages/api/events/add.ts
+++ src/pages/api/events/add.ts
@@ -26,8 +26,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 eventAddress: eventAddress
             },
         })
-        /* axios.post("https://fcm.googleapis.com/fcm/send", {
-            "registration_ids": ["e-EXD6aIQd-qPw0RbsL9tc:APA91bExMyw7jDrRtG3NMyBusO_9cSMuIIfohnn3rkUreXIM6yobdRs3ZuAYoGiq3Zp-YZgznPGgeCjEQiGLyYIditJFPw09fb5zkK8lF7tcYmXDEGb5OTOhQdM0NadqATrHJ5KTSFT2"],
+        axios.post("https://fcm.googleapis.com/fcm/send", {
+            "to": "topics/all",
             "notification": {
                 "body": "Body of Your Notification",
                 "title": "Title of Your Notification"
@@ -35,7 +35,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         }, {headers: {
             "Content-Type": "application/json",
             Authorization: "key= AAAAy-GxA_w:APA91bHmQzs-al_FRXiFt8djj7O0HN92TJXUN6gvoUTwJ3UdNO0m0aEFMW1xcGRaouhU0s6zCKxnifPUumiBKdpMN4GPYqkRoNVqrf0xDZe_RY_Gy8OjtBevpOLwKmVM8PbMHk1dinez"
-        }}) */
+        }})
         return res.status(200).json(createResponseData("Event successfully added!"));
     } catch (error) {
         return res.status(500).json({ error: error })
