da91a8c6bc6b250f8187bd6857b647b771518099
Erhan Gocen <71042486+erhangocen@users.noreply.github.com>
2023-07-14 06:47:24 +0300

    add following notification

--- src/pages/api/follows/add.ts
+++ src/pages/api/follows/add.ts
@@ -2,6 +2,7 @@ import { withMethods } from "@/lib/api-middlewares/with-methods";
 import { db } from "@/lib/db";
 import RequestMethods from "@/lib/request_methods";
 import createResponseData from "@/types/ResponseModel";
+import axios from "axios";
 import { group } from "console";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -18,6 +19,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
                 followeeId: followeeId,
                 followerId: followerId,
             },
+        })
+        var follower = await db.user.findUnique({
+            where: {
+                id: followerId
+            }
+        })
+        axios.post("https://fcm.googleapis.com/fcm/send", {
+            "to": "/topics/" + followeeId,
+            "notification": {
+                "body": follower?.fullName + " has followed you!",
+                "title": "You Have a New Follower!",
+            }
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "key= AAAAy-GxA_w:APA91bHmQzs-al_FRXiFt8djj7O0HN92TJXUN6gvoUTwJ3UdNO0m0aEFMW1xcGRaouhU0s6zCKxnifPUumiBKdpMN4GPYqkRoNVqrf0xDZe_RY_Gy8OjtBevpOLwKmVM8PbMHk1dinez"
+            }
         })
         return res.status(200).json(response);
     } catch (error) {
